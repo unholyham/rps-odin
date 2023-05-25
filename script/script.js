108f58af60b4519931e26281b940d9ce38893e3b
Ham <khairuddinumar@gmail.com>
2023-05-25 00:15:29 -0400

    Added choices for computer

--- script/script.js
+++ script/script.js
@@ -1,5 +1,20 @@
 getComputerChoice = () => {
-  console.log(Math.floor(Math.random() * 3));
+  let random = Math.floor(Math.random() * 3);
+  let choice = "";
+  switch (random) {
+    case 0:
+      choice = "Rock";
+      break;
+    case 1:
+      choice = "Paper";
+      break;
+    case 2:
+      choice = "Scissor";
+      break;
+  }
+  console.log(choice);
 };
+
+playRound();
 
 getComputerChoice();
